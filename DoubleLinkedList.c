051d5ed209e58c7485dfcfac77bce6499805b9ea
Paulo Sérgio Pierdoná <109459945+SrPatsu21@users.noreply.github.com>
2023-11-05 14:35:42 -0300

    Add files via upload

---
+++ DoubleLinkedList.c
@@ -0,0 +1,343 @@
+#ifndef DoubleLinkedList
+#define DoubleLinkedList
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define NULL ((void *)0)
+#define NAME_SIZE 16
+//struct
+//node
+typedef struct SNode
+{
+    struct SNode* prev;
+    char name[NAME_SIZE];
+    struct SNode* prox;
+}NODE;
+//list
+typedef struct SList{
+    struct SNode* head;
+    struct SNode* tail;
+    int size;
+}LIST;
+//errs
+void getOverflowErr()
+{
+    printf("\nOverflow: exceeded the device's storage capacity!\n");
+}
+void getUnderflowErr()
+{
+    printf("\nUnderflow: list is NULL\n");
+}
+void getPivotErr()
+{
+    printf("\npivot is the last one or list empty\n");
+}
+//alloc LIST
+LIST* createList()
+{
+    LIST * list =(LIST*) malloc(sizeof(LIST));
+    if (list != NULL)
+    {
+        list->head = NULL;
+        list->tail = NULL;
+        list->size = 0;
+        return list;
+    }else
+    {
+        return NULL;
+    }
+}
+//alloc node
+NODE* createNode()
+{
+    NODE* node;
+    node =(struct SNode*) malloc(sizeof(NODE));
+    if (node != NULL)
+    {
+        node->prev= NULL;
+        node->prox = NULL;
+        for (int i = 0; i < NAME_SIZE; i++)
+        {
+            node->name[i] = '\0';
+        }
+        
+        return node;
+    }else
+    {
+        return NULL;
+    }
+}
+
+//Node operations
+/*
+pivot is a reference to where place or remove
+*/
+
+//add node on list
+int addNode(LIST* list, char name [NAME_SIZE], NODE* pivot)
+{
+    NODE* node = createNode();
+    if (node != NULL)
+    {
+            strcpy(node->name, name);
+            if(pivot == NULL)
+            {
+                if(list->size == 0)
+                {
+                    list->tail = node;
+                }else
+                {
+                    list->head->prev = node;
+                }
+                node->prox = list->head;
+                list->head = node;
+            }else
+            {
+                node->prev = pivot;
+                node->prox = pivot->prox;
+                if (pivot->prox == NULL)
+                {
+                    list->tail = node;
+                }else
+                {
+                    node->prox->prev = node;
+                }
+                pivot->prox = node;
+            }
+        list->size++;
+        return 1;
+    }else
+    {
+        getOverflowErr();
+        return 0;
+    }
+    
+}
+//other add
+    //add head
+    int addHead(LIST* list, char name [NAME_SIZE])
+    {
+        return addNode(list, name, NULL);
+    }
+    //add tail
+    int addTail(LIST* list, char name [NAME_SIZE])
+    {
+        return addNode(list, name, list->tail);
+    }
+
+//to find some node using info
+NODE* findNode(LIST* list, char name [NAME_SIZE])
+{
+    NODE* node;
+    if (list->head != NULL)
+    {
+        node = list->head;
+        int i = 1;
+        while (i == 1)
+        {
+            if (node->name == name)
+            {
+                i=0;
+            }else if(node->prox == NULL)
+            {
+                node = NULL;
+                i=0;
+            }else
+            {
+                node = node->prox;
+            }
+        }      
+    }else
+    {
+        getUnderflowErr();
+        node = NULL;
+    }
+    return node;
+}
+//to find the prev of the node
+NODE* findNodePrev(LIST* list, char name [NAME_SIZE])
+{
+    NODE* node = findNode(list, name);
+    if (node == NULL)
+    {
+        return NULL;
+    }else{
+        return node->prev;
+    }
+}
+//remove node on list
+//in case of receiving parameter from find Pivot, if find Pivot don't find the pivot, will return NULL thats mean remove from head
+int removeNode(LIST* list, NODE* pivot)
+{
+    if(list->size == 0){
+        getUnderflowErr();
+        return 0;
+    }else
+    {
+        NODE* node;
+        if(pivot == NULL)
+        {
+            node = list->head;
+            list->head = node->prox;
+            if(node->prox == NULL)
+            {
+                list->tail = NULL;
+            }else
+            {
+                node->prox->prev = NULL;
+            }
+            free(node);
+            list->size--;
+            return 1;
+        }else
+        {
+            if(pivot->prox == NULL)
+            {
+                getPivotErr();
+                return 0;
+            }else
+            {
+                node = pivot->prox;
+                pivot->prox = node->prox;
+                if(node->prox == NULL)
+                {
+                    list->tail = pivot;
+                }else
+                {
+                    node->prox->prev = pivot;
+                }
+                free(node);
+                list->size--;
+                return 1;
+            }
+        }
+    }
+}
+//other remove
+    //remove head
+    int removeHead(LIST* list)
+    {
+        return removeNode(list, NULL);
+    }
+    //remove tail
+    int removeTail(LIST* list)
+    {
+        return removeNode(list, list->tail->prev);
+    }
+//print all values
+int runList(LIST* list)
+{
+    if (list->head != NULL)
+    {
+        NODE* node = list->head;
+        while (node != NULL)
+        {
+            printf("%s \n", node->name);
+                node = node->prox;
+        }
+        return 1;
+    }else
+    {
+        getUnderflowErr();
+        return 0;
+    }
+}
+//print all values end to start
+int runBackList(LIST* list)
+{
+    if (list->tail != NULL)
+    {
+        NODE* node = list->tail;
+        while (node != NULL)
+        {
+            printf("%s \n", node->name);
+                node = node->prev;
+        }
+    }else
+    {
+        getUnderflowErr();
+    }
+}
+//!important!
+//clear memory 
+int freeList(LIST* list)
+{
+    if (list->head != NULL)
+    {
+        NODE* node = list->head;
+        while (node != NULL)
+        {
+            NODE* aux = node;
+            node = node->prox;
+            free(aux);
+        }
+    }
+    free(list);
+    return 1;
+}
+//------------------------------------------------------------------------------------------------------------------------
+//smal or equal
+int isSmaller(NODE* ns, NODE* ne)
+{
+    if (ns != NULL && ne != NULL)
+    {
+        if (ns == ne)
+        {
+            return 1;
+        }else
+        {
+            int v1 = 0;
+            int v2 = 0;
+            for (int i = 0; i < NAME_SIZE && v1 == v2; i++)
+            {
+                v1 += ns->name[i];
+                v2 += ne->name[i];
+            }
+            if (v1 <= v2)
+            {
+                return 1;
+            }else
+            {
+                return 0;
+            }
+        }
+    }else
+    {
+        return 0;
+    }
+}
+//switch two values in an array
+void swap(NODE* i, NODE* j)
+{
+    char aux [NAME_SIZE];
+	strcpy(aux, i->name);
+	strcpy(i->name, j->name);
+	strcpy(j->name, aux);
+}
+//find pivot and swap
+NODE* partition(NODE* start, NODE* end)   
+{    
+    NODE *i = start->prev;   
+    for (NODE *j = start; j != end; j = j->prox)   
+    {   
+        if (isSmaller(j, end))   
+        {   
+            i = (i == NULL)? start : i->prox;   
+            swap(i, j);   
+        }   
+    }   
+    i = (i == NULL)? start : i->prox;
+    swap(i, end);   
+    return i;   
+}
+//sort list
+void quickSort(NODE* start, NODE* end)   
+{   
+    if (end != NULL && start != end && start != end->prox)   
+    {   
+        NODE* p = partition(start, end);   
+        quickSort(start, p->prev);   
+        quickSort(p->prox, end);   
+    }   
+}  
+#endif
---
+++ HasTable.c
@@ -0,0 +1,314 @@
+#include "DoubleLinkedList.c"
+#define COLUMNS 53
+#define MULTIPLY 367
+#define PATH "E:\\projetos\\projetosGit\\C\\hashtableworking\\nomes.txt"
+
+//columns
+typedef struct SCol
+{
+    struct SCol* prev;
+    struct SList* list;
+    int key;
+    struct SCol* prox;
+}COL;
+//hash table
+typedef struct SHashTable{
+    struct SCol* head;
+    struct SCol* tail;
+    int size;
+}HASHTABLE;
+//err
+void getInitializationErr()
+{
+    printf("\nIt is already defined\n");
+}
+void getFileErr()
+{
+    printf("File not found or can`t be open");
+}
+
+//create hashtable
+HASHTABLE* CreateTableHash()
+{
+    HASHTABLE* hash = (HASHTABLE*) malloc(sizeof(HASHTABLE));
+    if (hash != NULL)
+    {
+        hash->head = NULL;
+        hash->tail = NULL;
+        hash->size = 0;
+        return hash;
+    }else
+    {
+        return NULL;
+    }
+}
+//creating columns
+COL* createCol(int key)
+{
+    COL* col =(struct SCol*) malloc(sizeof(COL));
+    if (col != NULL)
+    {
+        col->prev= NULL;
+        col->prox = NULL;
+        col->list = createList();
+        col->key = key;
+        return col;
+    }else
+    {
+        return NULL;
+    }
+}
+//add columns
+int addColumns(HASHTABLE* hash, int key)
+{
+    COL* col = createCol(key);
+    if (col != NULL)
+    {
+        if(hash->tail == NULL)
+        {
+            col->prox = hash->tail;
+            col->prev = hash->head;
+            hash->head = col;
+            hash->tail = col;
+        }else
+        {
+            col->prev = hash->tail;
+            col->prox = hash->tail->prox;
+            hash->tail->prox = col;
+            hash->tail = col;
+        }
+        hash->size++;
+        return 1;
+    }else
+    {
+        getOverflowErr();
+        return 0;
+    }
+}
+//auto create hash table
+int autoDefineHashTable(HASHTABLE* hash, int Columns)
+{
+    if (hash->size == 0 && hash->head == NULL && hash->tail == NULL)
+    {
+        for (int i = 0; i < Columns; i++)
+        {
+            addColumns(hash, i);
+        }
+        return 1;
+    }else
+    {
+        getInitializationErr();
+        return 0;
+    }
+}
+//what key will be
+int hashing(HASHTABLE* hash, char name[NAME_SIZE], int size, int columns, int multiply)
+{
+    int key = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (name[i]!='\0')
+        {
+            key = ((multiply * key) + name[i]) % columns;
+        }
+    }
+    return key;
+}
+//find the key on hash table
+COL* findColumn(HASHTABLE* hash, int key)
+{
+    COL* col = hash->head;
+    while (col->key != key && col != NULL)
+    {
+        col = col->prox;
+    }
+    return col;
+}
+//add the name on the correct column
+int addOnColumn(COL* col, char name [NAME_SIZE])
+{
+    return addTail(col->list, name);
+}
+//add the name on the correct column
+int removeOnColumn(COL* col, char name [NAME_SIZE])
+{
+    return removeNode(col->list, findNodePrev(col->list, name));
+}
+//read the file
+FILE* loadingData(char name[])
+{
+    FILE* fptr = fopen(name, "r");
+    if (fptr != NULL)
+    {
+        return fptr;
+    }else
+    {
+        getFileErr();
+        return NULL;
+    }
+}
+//gets all names and put at the hash
+int autoHashData(FILE* fptr, HASHTABLE* hash, int size, int columns, int multiply){
+    if (fptr != NULL)
+    {
+        char name[NAME_SIZE];  
+        while(fgets(name, NAME_SIZE, fptr)) 
+        {
+            for (int i = 0; i < NAME_SIZE; i++)
+            {
+                if (name[i]=='\n' || name[i]==' ' || name[i]=='\t')
+                {
+                    name[i]='\0';
+                }
+            }
+            addOnColumn(findColumn(hash, hashing(hash, name, size, columns, multiply)), name);
+        }
+        return 1;
+    }else
+    {
+        return 0;
+    }
+
+}
+//see the size of all elements on hash table
+//print all values
+int runHashTable(HASHTABLE* hash)
+{
+    if (hash->head != NULL)
+    {
+        COL* col = hash->head;
+        while (col != NULL)
+        {
+            printf("%2i\t", col->key);
+            printf("%i\n", col->list->size);
+                col = col->prox;
+        }
+        return 1;
+    }else
+    {
+        getUnderflowErr();
+        return 0;
+    }
+}
+//see a list
+int seeHashList(HASHTABLE* hash, int key)
+{
+    printf("start\n");
+    COL* col = findColumn(hash, key);
+    runList(col->list);
+    printf("end\n");
+}
+//sort
+int sortColOnHashTable(HASHTABLE* hash)
+{
+    if (hash->head != NULL)
+    {
+        COL* col = hash->head;
+        while (col != NULL)
+        {
+            quickSort(col->list->head, col->list->tail);
+            col = col->prox;
+        }
+        return 1;
+    }else
+    {
+        getUnderflowErr();
+        return 0;
+    }
+}
+//!important!
+//clear memory
+int freeHash(HASHTABLE* hash)
+{
+    if (hash->head != NULL)
+    {
+        COL* col = hash->head;
+        while (col != NULL)
+        {
+            COL* aux = col;
+            freeList(col->list);
+            col = col->prox;
+            free(aux);
+        }
+    }
+    free(hash);
+    return 1;
+}
+void main()
+{
+    //init
+    int op = 1;
+    char name[NAME_SIZE];
+    HASHTABLE* hash = CreateTableHash(COLUMNS);
+    autoDefineHashTable(hash, COLUMNS);
+    //loading
+    FILE* fptr = loadingData(PATH);
+    autoHashData(fptr, hash, NAME_SIZE, COLUMNS, MULTIPLY);
+    fclose(fptr);
+    //sort
+    sortColOnHashTable(hash);
+    //view
+    while (op > 0)
+    {
+        printf("---------------------------\n");
+        printf("1.add a new name\n2.remove a name\n3.Sort all names \n4.See hash table size\n5.print a list\n6.find a name key\n0.close\nop:\n");
+        scanf("%i", &op);
+        printf("---------------------------\n");
+        if (op == 1)
+        {
+            name[0]= 'a';
+            printf("name:");
+            scanf(" %[^\n]s", name);
+            for (int i = 0; i < NAME_SIZE; i++)
+            {
+                if (name[i]=='\n' || name[i]==' ' || name[i]=='\t')
+                {
+                    name[i]='\0';
+                }
+            }
+            printf("key:%i\n", hashing(hash, name, NAME_SIZE, COLUMNS, MULTIPLY));
+            addOnColumn(findColumn(hash, hashing(hash, name, NAME_SIZE, COLUMNS, MULTIPLY)), name);
+        }else if (op == 2)
+        {
+            name[0]= 'a';
+            printf("name:");
+            scanf(" %[^\n]s", name);
+            for (int i = 0; i < NAME_SIZE; i++)
+            {
+                if (name[i]=='\n' || name[i]==' ' || name[i]=='\t')
+                {
+                    name[i]='\0';
+                }
+            }
+            printf("key:%i\n", findColumn(hash, hashing(hash, name, NAME_SIZE, COLUMNS, MULTIPLY)));
+            removeOnColumn(findColumn(hash, hashing(hash, name, NAME_SIZE, COLUMNS, MULTIPLY)), name);   
+        }else if(op == 3)
+        {
+            sortColOnHashTable(hash);
+        }else if (op == 4)
+        {
+            runHashTable(hash);
+        }else if(op == 5)
+        {
+            int key;
+            printf("key:");
+            scanf("%i", &key);
+            seeHashList(hash, key);
+        }else if(op == 6)
+        {
+            name[0]= 'a';
+            printf("name:");
+            scanf(" %[^\n]s", name);
+            for (int i = 0; i < NAME_SIZE; i++)
+            {
+                if (name[i]=='\n' || name[i]==' ' || name[i]=='\t')
+                {
+                    name[i]='\0';
+                }
+            }
+            printf("key:%i\n", findColumn(hash, hashing(hash, name, NAME_SIZE, COLUMNS, MULTIPLY)));
+        }
+    }
+    //close
+    freeHash(hash);
+}
